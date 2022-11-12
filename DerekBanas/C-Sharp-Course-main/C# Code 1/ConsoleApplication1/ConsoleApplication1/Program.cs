a14e2cc4ef032fbe0b94131a5596d25a96daa81d
Ray Andrade <TheRayCode@RayAndrade.com>
2022-11-12 00:31:31 -0800

    Derek Babanas

---
+++ DerekBanas/C-Sharp-Course-main/C# Code 1/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    //https://www.youtube.com/watch?v=M5ugY7fWydE
+    public class Program
+    {
+        static void Main(string [] args)
+        {
+            Console.WriteLine("The Ray Code is AWESOME!!!");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Green; 
+            Console.Clear();
+            //time : 8:24
+        }
+    }
+}
