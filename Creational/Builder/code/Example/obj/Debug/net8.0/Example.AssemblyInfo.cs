7cde49059558ff88052cfab0d32ad0e58d31972a
Ray Andrade <TheRayCode@RayAndrade.com>
2025-07-31 10:59:46 -0700

    *

--- Creational/Builder/code/Example/obj/Debug/net8.0/Example.AssemblyInfo.cs
+++ Creational/Builder/code/Example/obj/Debug/net8.0/Example.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Example")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4673a97cbe0880bfd76ecf3daff6b27fb23e68d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+457bb6a5afee4c67d275f82327f8a1ba349e0690")]
 [assembly: System.Reflection.AssemblyProductAttribute("Example")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Example")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
