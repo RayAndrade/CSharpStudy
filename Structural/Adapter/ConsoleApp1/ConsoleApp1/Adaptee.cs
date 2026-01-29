6c99d4f8a15cc312403b321bd16b8a7d875919ec
Ray Andrade <TheRayCode@RayAndrade.com>
2026-01-28 20:14:27 -0800

    *

---
+++ Structural/Adapter/ConsoleApp1/ConsoleApp1/Adaptee.cs
@@ -0,0 +1,9 @@
+namespace ConsoleApp1;
+
+public class Adaptee
+{
+    public string GetSpecificRequest()
+    {
+        return "Specific request.";
+    }
+}
---
+++ Structural/Adapter/ConsoleApp1/ConsoleApp1/ITarget.cs
@@ -0,0 +1,6 @@
+namespace ConsoleApp1;
+
+public interface ITarget
+{
+    string GetRequest();
+}
